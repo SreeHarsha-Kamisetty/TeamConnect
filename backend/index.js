5928d7b3c7bb86a332ea04db4dbb5ac143104bbf
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-02-21 19:25:05 +0530

    User Router created

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan");
 const { DBConnection } = require("./db");
+const { UserRouter } = require("./routes/user.routes");
 const PORT = process.env.PORT || 8080
 
 const app = express();
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
+app.use("/users",UserRouter)
 
 app.get("/",(req,res)=>{
     res.send("Home")
