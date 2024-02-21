0bdca725cdad0b785ca92fc5815123cd8a69d86c
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-02-21 07:17:46 +0530

    basic express app created

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,18 @@
-console.log("hello")
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const PORT = process.env.PORT || 8080
+
+const app = express();
+app.use(cors())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+
+
+app.get("/",(req,res)=>{
+    res.send("Home")
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server running at http://localhost:${PORT}`)
+})
