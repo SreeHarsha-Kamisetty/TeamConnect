34c03b6c3a347635bed4e24e7060144ef2ddf864
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-02-21 19:21:00 +0530

    Created user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,1 +1,28 @@
 // User Model
+
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  userName: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  workspaces:[
+    {
+        id:String,
+        workspacename:String
+    }
+  ]
+});
+
+const UserModel = mongoose.model("user",userSchema)
+
+
+module.exports={
+    UserModel
+}
