2c7f90ebfd231ef33683fa0f11ec8a8e4f7df15a
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-02-22 07:18:37 +0530

    created: authentication middleware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,1 +1,22 @@
 // Authentication Middleware
+
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+
+
+const auth = async(req,res,next)=>{
+    try {
+        let access_token = req.headers.authorization?.split(" ")[1];
+        if(!access_token) return res.status(401).json({Message:"Please login"})
+
+        let decoded_token =  jwt.verify(access_token,process.env.SECRET_KEY)
+
+        req.body.userID = decoded_token.userID
+        req.body.userName = decoded_token.userName
+        next();
+
+
+    } catch (error) {
+        res.status(403).json({Error:"Unauthorized"})
+    }
+}
