a6aa68319abf07eff6d9464e71dc83de104f21e7
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-02-22 17:42:49 +0530

    updates: set user image a default value

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -12,7 +12,10 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
   },
-  image:String,
+  image:{
+    type:String,
+    default:"https://firebasestorage.googleapis.com/v0/b/coinsquare-8dc2e.appspot.com/o/default.jpg?alt=media&token=fa163076-3ed8-48b2-875b-3b370c66f251"
+},
   workspaces:[
     {
         id:String,
