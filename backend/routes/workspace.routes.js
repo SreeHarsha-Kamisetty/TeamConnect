02127db1626123be3b850d3a48d267b49f6cdbaf
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-02-25 17:39:50 +0530

    created: workspace router

---
+++ backend/routes/workspace.routes.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const { WorkspaceModel } = require("../models/workspace.model");
+const {UserModel} = require("../models/user.model")
+const WorkspaceRouter = express.Router();
+
+WorkspaceRouter.get("/list",async(req,res)=>{
+    try {
+        
+        let workspace_list = await WorkspaceModel.find();
+        
+        res.status(200).json({workspace_list})
+    } catch (error) {
+        res.status(400).json({Error:"Error during getting workspace list"})
+    }
+})
+WorkspaceRouter.post("/create",async(req,res)=>{
+    try {
+        let user = await UserModel.findOne({_id:req.body.userId})
+        let user_details={
+            userName:user.userName,
+            userImage:user.image,
+            userId:user._id
+        }
+        let workspace_details = {
+            workspaceName:req.body.workspaceName,
+            channels:[
+                {
+                    channelName:"general"
+                }
+            ],
+            users:[
+                user_details
+            ]
+        }
+        let new_workspace = new WorkspaceModel(workspace_details)
+        await new_workspace.save();
+
+        res.status(200).json({Message:"New Workspace created",new_workspace})
+    } catch (error) {
+        res.status(400).json({Error:"Error creating workspace"})
+    }
+})
+
+
+WorkspaceRouter.patch("/add/users/:workspaceId",async(req,res)=>{
+    try {
+        let {workspaceId} = req.params
+
+        let workspace = await WorkspaceModel.findOne({_id:workspaceId})
+
+        if(!workspace) return res.status(404).json({Error:"Wrokspace not found"})
+        let {email} = req.body
+        let user = await UserModel.findOne({email:email})
+        let userData = {
+            userId:user._id,
+            userName:user.userName,
+            userImage:user.image
+        }
+        let user_list = workspace.users
+        if(!user_list.includes(userData)){
+            user_list.push(userData)
+        }
+        
+        await WorkspaceModel.findByIdAndUpdate({_id:workspaceId},{users:user_list})
+        res.status(200).json({Message:"User added"})
+    } catch (error) {
+        res.status(400).json("Error")
+    }
+})
+
+module.exports={
+    WorkspaceRouter
+}
