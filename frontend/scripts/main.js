7428cbd020fbfe779b10936f4c77dc80be54a7bd
aditi-keerti <aditibhadoriya1333@gmail.com>
2024-02-25 17:25:10 +0530

    Feature:profile edit

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,9 +1,4 @@
-// Socket.IO connection with user information
-// const socket = io('https://teamconnect.onrender.com', {
-//     query: {
-//         userId: localStorage.getItem('userName') // Use the user ID or any unique identifier
-//     }
-// });
+
 
 const socket = io('https://teamconnect.onrender.com',{transports:["websocket"], query: {
             userId: localStorage.getItem('userName') // Use the user ID or any unique identifier
@@ -22,29 +17,6 @@ if(img){
     imgSpan.innerHTML="";
     imgSpan.innerHTML=`<img id="img-tag" src=${img}>`
 }
-// const logout = document.getElementById('logout');
-// logout.addEventListener('click', () => {
-//     // Make a request to the server to clear the session or delete the cookies
-//     fetch('http://localhost:4000/users/logout', {
-//         method: 'POST', // Use the appropriate HTTP method for your server
-//         credentials: 'include', // Include credentials (cookies) in the request
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//     })
-//     .then(res => res.json())
-//     .then(data => {
-//         if (data.mesg === "logout successful") {
-//             // Clear local storage or perform other client-side cleanup
-//             localStorage.clear();
-//             // Redirect to the login page
-//             window.location.href = '../../frontend/login.html';
-//         }
-//     })
-//     .catch(error => {
-//         console.error('Error during logout:', error);
-//     });
-// });
 
 
 
@@ -136,8 +108,8 @@ function clearFeedback(){
 // Room name
 
 // Prompt the user to enter a room
-const room = prompt('Enter a room name:');
-socket.emit('join room', room);
+// const room = prompt('Enter a room name:');
+// socket.emit('join room', room);
 
 // Listen for chat messages
 socket.on('chat message', (msg) => {
@@ -154,3 +126,101 @@ socket.on('chat message', (msg) => {
 //   }
 //   return false;
 // });
+
+
+// modal js
+function showModal() {
+    // Assuming your modal has an ID of "myModal"
+    const id=document.getElementById('myModal')
+    const myModal = new bootstrap.Modal(id);
+    myModal.show();
+}
+
+const prof_btn = document.getElementById('prof-btn');
+prof_btn.addEventListener('click', () => {
+    const profile = new bootstrap.Modal(document.getElementById('profile-model'));
+    profile.show();
+    console.log('click');
+});
+
+//image upload js
+let upload_img_form = document.getElementById("upload-img-form");
+let save_img = document.getElementById("save-img");
+save_img.addEventListener("click",async()=>{
+    let new_image = document.getElementById("new-profile-image")
+    let image = new_image.files[0];
+    let data = new FormData();
+    data.append('image',image)
+    
+    try{
+        let res = await fetch(`https://coinsquare.onrender.com/user/profile/${localStorage.getItem('id')}`,{
+            method:"PATCH",
+            headers:{
+                "enctype":"multipart/form-data"
+            },
+            body:data
+        })
+        let new_data = await res.json();
+        console.log(new_data.updated);
+        let test_img = document.getElementById("test-img")
+        test_img.src = new_data.updated
+        let profile_pic = document.getElementById("profile-pic")
+        profile_pic.src = new_data.updated
+        // localStorage.removeItem("image");
+        // localStorage.setItem("image",new_data.updated);
+        window.location.href="./dashboard.html"
+    }
+    catch(error){
+        console.log(error)
+    }
+})
+upload_img_form.addEventListener("submit",(e)=>{
+    const profile = new bootstrap.Modal(document.getElementById('profile-model'));
+    e.preventDefault();
+    profile.show(); 
+})
+
+// load image on page load
+async function loadImage(id){
+    try {
+        let res = await fetch(`https://coinsquare.onrender.com/user/profile/${id}`)
+        let data = await res.json();
+        console.log(data);
+        let profile = data.image
+        let test_img = document.getElementById("test-img")
+            test_img.src = profile
+            let profile_pic = document.getElementById("profile-pic")
+            profile_pic.src = profile
+    } catch (error) {
+        console.log(error);
+    }
+  
+}
+loadImage(id);
+
+
+// logout fuctionalities
+const logout = async () => {
+    try {
+      const response = await fetch('https://coinsquare.onrender.com/user/logout', {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`, // Include the access token if needed
+        },
+      });
+  
+      if (response.ok) {
+        // Handle successful logout
+        console.log("Logout successful");
+        window.location.href = '../index.html';
+      } else {
+        // Handle error response
+        const data = await response.json();
+        console.error("Logout failed:", data.err);
+      }
+    } catch (error) {
+      console.error("Error during logout:", error);
+    }
+  };
+  // Example button click event
+document.getElementById("logout-btn").addEventListener("click", logout);
